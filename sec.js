db1094d5efae6aa24242ab5d2b3d5cef0d64bb34
bandiEswar <121429247+bandiEswar@users.noreply.github.com>
2023-07-19 19:33:13 +0530

    Sec.html

---
+++ sec.js
@@ -0,0 +1,9 @@
+function Mor(){
+    document.getElementById("sun").src="https://vectorified.com/image/rising-sun-logo-vector-1.jpg";
+}
+function Afn(){
+    document.getElementById("sun").src="https://tse1.mm.bing.net/th?id=OIP.ObkDOYMpITLIHEWze_U3FQHaGf&pid=Api&P=0&h=180";
+}
+function Evn(){
+    document.getElementById("sun").src="https://clipground.com/images/evening-sun-clipart-11.jpg";
+}
